9ea3abee6e7b5c45f5158b6478a34782288646b9
Rizki Romadhoni <rrmdn@pm.me>
2021-05-06 10:14:59 +0700

    feature: clone a context with a different initial state

--- src/index.spec.tsx
+++ src/index.spec.tsx
@@ -46,4 +46,35 @@ describe("immersive", () => {
 
     expect(ctx.current.tasks.length).toBe(1);
   });
+
+  it("should be able to clone a context with a different initial state", () => {
+    const context = createContext(
+      { tasks: [{ task: "hello", done: false }] },
+      (modify) => ({
+        addTask: (task: string) => {
+          modify((draft) => {
+            draft.tasks.push({ done: false, task });
+          });
+        },
+      })
+    );
+
+    const clonedContext = context.clone({
+      tasks: [{ task: "world", done: true }],
+    });
+
+    const { result: ctx } = renderHook(
+      () => ({
+        actions: clonedContext.useActions(),
+        tasks: clonedContext.useSelectState((data) => data.tasks),
+      }),
+      {
+        wrapper: ({ children }) => (
+          <context.Provider>{children}</context.Provider>
+        ),
+      }
+    );
+    expect(ctx.current.tasks[0].task).toBe("world");
+    expect(typeof ctx.current.actions.addTask).toBe("function");
+  });
 });
